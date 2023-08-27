410d65a4322c35e8167b84933f2b6128530106ee
Dalon <dalon693@gmail.com>
2023-08-27 13:46:12 +0300

    Input

---
+++ Assets/Feature/Input/AbstractInput.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Jam.Input
+{
+    public abstract class AbstractInput : MonoBehaviour
+    {
+        
+
+    }
+}
