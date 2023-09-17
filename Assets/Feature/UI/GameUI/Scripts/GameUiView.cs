9d984feddc5433922adc9a0256b349db7b3912d8
Dalon <dalon693@gmail.com>
2023-09-17 11:29:57 +0300

    Чистка

---
+++ Assets/Feature/UI/GameUI/Scripts/GameUiView.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Jam.UI.GameUI
+{
+    public class GameUiView : MonoBehaviour
+    {
+        [SerializeField] private Image imageHealth;
+        [SerializeField] private Image imageTime;
+
+
+        public void ShowHealth(float value)
+        {
+
+        }
+        public void ShowTime(float value)
+        {
+
+        }
+
+    }
+}
